8bfc3831ce841aeb707e341af1e6a99b515ee6ce
ElviraIlina <xv1px2019@gmail.com>
2026-07-02 23:58:43 +0300

    elv

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -1,6 +1,6 @@
 const galleryPhotos = [
   {
-    "src": "photos/commercial/001-2024-04-15-14-31-03.webp",
+    "src": "photos/commercial/013-2024-10-24-20-56-37.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "commercial"
   },
@@ -10,14 +10,14 @@ const galleryPhotos = [
     "category": "commercial"
   },
   {
-    "src": "photos/commercial/023-2025-03-02-20-59-44_1740971691848.jpg",
+    "src": "photos/other/003-2024-04-18-15-24-00.webp",
     "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
+    "category": null
   },
   {
-    "src": "photos/commercial/013-2024-10-24-20-56-37.jpg",
+    "src": "photos/commercial/023-2025-03-02-20-59-44_1740971691848.jpg",
     "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
+    "category": "nude"
   },
   {
     "src": "photos/commercial/047-2025-12-10-11-18-11.jpg",
@@ -25,9 +25,9 @@ const galleryPhotos = [
     "category": "commercial"
   },
   {
-    "src": "photos/other/003-2024-04-18-15-24-00.webp",
+    "src": "photos/nude/img_7064.jpg",
     "caption": "ELVIRAILINA MAKEUP",
-    "category": null
+    "category": "nude"
   },
   {
     "src": "photos/other/004-2024-04-18-15-26-53.webp",
@@ -35,52 +35,12 @@ const galleryPhotos = [
     "category": null
   },
   {
-    "src": "photos/creative/006-2024-04-18-15-50-27.webp",
+    "src": "photos/creative/034-2025-07-25-14-53-18.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
   },
   {
-    "src": "photos/creative/030-2025-07-10-16-31-14.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/015-2024-11-01-22-41-54.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/016-2024-11-29-16-42-14.webp",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/commercial/002-2024-04-15-15-49-19.webp",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/creative/011-2024-10-01-19-10-44.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/008-2024-06-12-18-49-35.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/017-2024-11-29-16-42-40.webp",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/007-2024-06-12-15-02-27.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/019-2025-01-31-12-12-25.jpg",
+    "src": "photos/creative/035-2025-07-25-15-01-55.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
   },
@@ -90,7 +50,117 @@ const galleryPhotos = [
     "category": "creative"
   },
   {
+    "src": "photos/creative/019-2025-01-31-12-12-25.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/006-2024-04-18-15-50-27.webp",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/commercial/002-2024-04-15-15-49-19.webp",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "commercial"
+  },
+  {
+    "src": "photos/creative/016-2024-11-29-16-42-14.webp",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/017-2024-11-29-16-42-40.webp",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/011-2024-10-01-19-10-44.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/024-2025-04-16-19-33-46.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/hair/049-2025-12-22-19-56-34.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "hair"
+  },
+  {
+    "src": "photos/creative/008-2024-06-12-18-49-35.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/015-2024-11-01-22-41-54.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/007-2024-06-12-15-02-27.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/creative/030-2025-07-10-16-31-14.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/nude/012-2024-10-04-11-33-17.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "nude"
+  },
+  {
     "src": "photos/creative/021-2025-02-02-12-59-28.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/commercial/014-2024-10-24-20-58-18.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "commercial"
+  },
+  {
+    "src": "photos/hair/025-2025-04-18-07-37-16.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "hair"
+  },
+  {
+    "src": "photos/hair/img_7050.png",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "hair"
+  },
+  {
+    "src": "photos/hair/img_7049.png",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "hair"
+  },
+  {
+    "src": "photos/commercial/img_6490.png",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "commercial"
+  },
+  {
+    "src": "photos/creative/022-2025-03-01-23-30-43_1740927587631.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/other/037-2025-08-03-18-41-14.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": null
+  },
+  {
+    "src": "photos/commercial/070-img_5217.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "commercial"
+  },
+  {
+    "src": "photos/creative/img_7041.png",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
   },
@@ -105,76 +175,6 @@ const galleryPhotos = [
     "category": "creative"
   },
   {
-    "src": "photos/nude/012-2024-10-04-11-33-17.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "nude"
-  },
-  {
-    "src": "photos/creative/022-2025-03-01-23-30-43_1740927587631.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/hair/025-2025-04-18-07-37-16.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
-  },
-  {
-    "src": "photos/commercial/014-2024-10-24-20-58-18.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/hair/029-2025-06-11-20-47-19.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
-  },
-  {
-    "src": "photos/creative/024-2025-04-16-19-33-46.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/hair/031-2025-07-10-16-37-35.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
-  },
-  {
-    "src": "photos/commercial/img_6490.png",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/creative/img_7041.png",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/034-2025-07-25-14-53-18.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/other/037-2025-08-03-18-41-14.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": null
-  },
-  {
-    "src": "photos/commercial/028-2025-05-13-17-40-53_1747389707080.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/creative/035-2025-07-25-15-01-55.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/other/043-2025-10-30-23-22-39.webp",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": null
-  },
-  {
     "src": "photos/creative/044-2025-10-31-09-54-58.webp",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
@@ -187,27 +187,12 @@ const galleryPhotos = [
   {
     "src": "photos/hair/046-2025-12-03-00-59-57.jpg",
     "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
-  },
-  {
-    "src": "photos/commercial/032-2025-07-17-21-01-46.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/commercial/018-20241216-dsc03068.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/hair/049-2025-12-22-19-56-34.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
-  },
-  {
-    "src": "photos/nude/054-dsc02511.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
     "category": "nude"
+  },
+  {
+    "src": "photos/commercial/071-img_5450.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "commercial"
   },
   {
     "src": "photos/commercial/039-2025-10-08-14-09-03.jpg",
@@ -215,17 +200,27 @@ const galleryPhotos = [
     "category": "commercial"
   },
   {
-    "src": "photos/nude/050-2025-12-22-22-01-30.jpg",
+    "src": "photos/commercial/001-2024-04-15-14-31-03.webp",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "nude"
   },
   {
-    "src": "photos/commercial/055-dsc02843.jpg",
+    "src": "photos/commercial/028-2025-05-13-17-40-53_1747389707080.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "commercial"
   },
   {
-    "src": "photos/commercial/071-img_5450.jpg",
+    "src": "photos/nude/054-dsc02511.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "nude"
+  },
+  {
+    "src": "photos/commercial/018-20241216-dsc03068.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/commercial/032-2025-07-17-21-01-46.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "commercial"
   },
@@ -255,22 +250,17 @@ const galleryPhotos = [
     "category": "hair"
   },
   {
-    "src": "photos/hair/img_7050.png",
+    "src": "photos/hair/029-2025-06-11-20-47-19.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "hair"
   },
   {
-    "src": "photos/hair/img_7049.png",
+    "src": "photos/hair/031-2025-07-10-16-37-35.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "hair"
   },
   {
     "src": "photos/creative/065-img_2061.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
-    "src": "photos/creative/063-img_0638.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
   },
@@ -285,12 +275,17 @@ const galleryPhotos = [
     "category": "creative"
   },
   {
-    "src": "photos/commercial/070-img_5217.jpg",
+    "src": "photos/creative/073-img_6919.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/commercial/068-img_3942.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "commercial"
   },
   {
-    "src": "photos/creative/073-img_6919.jpg",
+    "src": "photos/creative/img_7037.png",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
   },
@@ -300,29 +295,14 @@ const galleryPhotos = [
     "category": "commercial"
   },
   {
-    "src": "photos/nude/053-dsc02503.jpg",
+    "src": "photos/nude/050-2025-12-22-22-01-30.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "nude"
   },
   {
-    "src": "photos/nude/056-dsc02959.jpg",
+    "src": "photos/nude/img_4546.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "nude"
-  },
-  {
-    "src": "photos/hair/067-img_2935.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
-  },
-  {
-    "src": "photos/nude/057-dsc03024.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "nude"
-  },
-  {
-    "src": "photos/hair/img_6642.png",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "hair"
   },
   {
     "src": "photos/hair/062-img_0388.jpg",
@@ -331,6 +311,11 @@ const galleryPhotos = [
   },
   {
     "src": "photos/hair/img_7031.png",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "hair"
+  },
+  {
+    "src": "photos/hair/img_6642.png",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "hair"
   },
@@ -370,11 +355,6 @@ const galleryPhotos = [
     "category": "hair"
   },
   {
-    "src": "photos/creative/img_7037.png",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "creative"
-  },
-  {
     "src": "photos/creative/img_7040.png",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "creative"
@@ -390,32 +370,17 @@ const galleryPhotos = [
     "category": "creative"
   },
   {
-    "src": "photos/nude/img_7064.jpg",
+    "src": "photos/creative/063-img_0638.jpg",
+    "caption": "ELVIRAILINA MAKEUP",
+    "category": "creative"
+  },
+  {
+    "src": "photos/nude/056-dsc02959.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "nude"
   },
   {
-    "src": "photos/commercial/027-2025-05-02-11-46-11.webp",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/commercial/051-dsc01574.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/commercial/052-dsc01695.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/commercial/068-img_3942.jpg",
-    "caption": "ELVIRAILINA MAKEUP",
-    "category": "commercial"
-  },
-  {
-    "src": "photos/commercial/IMG_2042.JPG",
+    "src": "photos/commercial/055-dsc02843.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "commercial"
   },
@@ -435,7 +400,7 @@ const galleryPhotos = [
     "category": null
   },
   {
-    "src": "photos/nude/img_4546.jpg",
+    "src": "photos/nude/057-dsc03024.jpg",
     "caption": "ELVIRAILINA MAKEUP",
     "category": "nude"
   }
